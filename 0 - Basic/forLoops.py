062964a2912a0ee6152b3ff47991321478623749
Omar Khan <major.viewer@gmail.com>
2018-07-21 23:28:23 -0400

    for loop updated

--- 0 - Basic/forLoops.py
+++ 0 - Basic/forLoops.py
@@ -3,7 +3,18 @@ myList = [1,2,3]
 
 for i in range(5):
 	print("FIRST LOOP!")
+
 for x in range(0, 5):
 	print("SECOND LOOP!")
+
 for j in myList:
 	print("THIRD LOOP!")
+
+for item in myList:
+	print("FOURTH LOOP!")
+	if(item == 2):
+		break
+
+for t in range(0, 10, 2):
+	print(t, "FIFTH LOOP!")
+

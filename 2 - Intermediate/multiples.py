31f7c2fe14d853e90ffe4e70173aacce1df5e03f
Omar Khan <major.viewer@gmail.com>
2018-11-10 16:13:05 -0500

    Python Projects

--- 2 - Intermediate/multiples.py
+++ 2 - Intermediate/multiples.py
@@ -1,12 +1,12 @@
 saveList = []
-
 def sumFunc(n):
 	counter = 0
 	while counter < n:
 		if counter % 3 == 0 or counter % 5 == 0:
-			print(counter)
 			saveList.append(counter)
 		counter = counter + 1
 	return sum(saveList)
-			
 print(sumFunc(1000))
+
+# One line solution
+# print(sum([i for i in range(1000) if i%3==0 or i%5==0]))
